7a5af0b063a66fbf1cc4c901f0eb29ae666984a6
Ezequiel <ezequiel.santos@btgpactual.com>
2017-03-21 12:47:09 -0300

    reoganizacao das pastas

--- Aplicativo/webapp/hello.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $.ajax({
-        url: "sua-url-do-json"
-    }).then(function(data) {
-       $('.valor-1').append(data.id);
-       $('.valor-2').append(data.content);
-    });
-});
